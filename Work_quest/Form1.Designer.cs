befb7ec6f9d5d5021b411e6a9a170bc88a705994
Alitakaruka <galashevich69@mail.ru>
2020-11-05 08:28:23 +0300

    да

---
+++ Work_quest/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Work_quest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(421, 456);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добрый день,астронавты!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(395, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(220, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выберите название генератора";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1 генератор";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(120, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(237, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Код каждого реактора";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(607, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(53, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(607, 144);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 36);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Принять код";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(696, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 22);
+            this.textBox2.TabIndex = 8;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(443, 70);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(147, 21);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Генератор Альфа";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(443, 116);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(143, 21);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Генератор Бетта";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(443, 159);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(146, 21);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Генератор Гамма";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(608, 230);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(52, 22);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(696, 230);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(52, 22);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(608, 323);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 47);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Рассчитать версию";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(120, 176);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(75, 22);
+            this.textBox5.TabIndex = 15;
+            this.textBox5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(454, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(475, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Теперь введите прошлую и будущую версию прошивки (Любые числа)";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 118);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 17);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Название прошивки:";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 17);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Версия:";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(174, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 17);
+            this.label7.TabIndex = 19;
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 260);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(78, 17);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Описание:";
+            this.label8.Visible = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(154, 260);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 17);
+            this.label9.TabIndex = 21;
+            this.label9.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1115, 482);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Помошник по прошивке";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Work_quest/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using lib.dll;
+
+namespace Work_quest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("16,48","Код 1.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("24,71", "Код 2.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("92,32", "Код 3.");
+        }
+
+        public void button4_Click(object sender, EventArgs e)
+        {
+          
+
+            if (radioButton1.Checked)
+            {
+          
+                if (textBox1.Text =="24" && textBox2.Text == "71") // Проверка правильности кода
+                {
+                    textBox3.Visible = true;
+                    textBox4.Visible = true;
+                    button5.Visible = true;
+                    button4.Visible = false;
+                    textBox1.Visible = false;
+                    textBox2.Visible = false;
+                    label4.Visible = true;
+                }
+                else
+                {
+                    label1.Text = "Неправильный код!";
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                }
+               
+                    
+                finaly name1  = new finaly();
+                name1.PluginName = "PrototypeA";
+                label7.Text = name1.PluginName;
+                name1.Description = "Лучшая версия прошивки генератора Альфа.";
+                label9.Text = name1.Description;
+                textBox5.Text = "A";
+            }
+
+            if (radioButton2.Checked)
+            {
+
+                if (textBox1.Text == "92" && textBox2.Text == "32") // Проверка правильности кода
+                {
+                    textBox3.Visible = true;
+                    textBox4.Visible = true;
+                    button5.Visible = true;
+                    button4.Visible = false;
+                    textBox1.Visible = false;
+                    textBox2.Visible = false;
+                    label4.Visible = true;
+                }
+                else
+                {
+                    label1.Text = "Неправильный код!";
+                    textBox1.Text = "";
+                    textBox2.Text = "";
+                }
+
+
+                finaly name2 = new finaly();
+                name2.PluginName = "PrototypeB";
+                label7.Text = name2.PluginName;
+                name2.Description = "Лучшая версия прошивки генератора Бетта.";
+                label9.Text = name2.Description;
+                textBox5.Text = "B";
+            }
+                if (radioButton3.Checked)
+                {
+               
+                    if (textBox1.Text == "16" && textBox2.Text == "48") // Проверка правильности кода
+                    {
+                        textBox3.Visible = true;
+                        textBox4.Visible = true;
+                        button5.Visible = true;
+                        button4.Visible = false;
+                        textBox1.Visible = false;
+                        textBox2.Visible = false;
+                        label4.Visible = true;
+                    }
+                    else
+                    {
+                        label1.Text = "Неправильный код!";
+                        textBox1.Text = "";
+                        textBox2.Text = "";
+                    
+                    }
+                    finaly name3 = new finaly();
+                    name3.PluginName = "PrototypeG";
+                    label7.Text = name3.PluginName;
+                    name3.Description = "Лучшая версия прошивки генератора Гамма.";
+                    label9.Text = name3.Description;
+                textBox5.Text = "G";
+                }
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text == ("") || textBox4.Text == (""))
+            {
+                label1.Text = "Нет кода!";
+            }
+            int kode1 = Convert.ToInt32(textBox3.Text);// Конвертирование  в число
+            int kode2 = Convert.ToInt32(textBox4.Text);
+
+            finaly version = new finaly(); //Обращаюсь к библеотеке (lib.dll) и исполбзую метод Run
+           
+            version.Version = Convert.ToString (version.Run(kode1, kode2)); //Вставляю на место input1 и input2 переменные со значением
+                                                                            
+            textBox5.Text += version.Version;
+            label5.Visible = true;
+            label7.Visible = true;
+            label9.Visible = true;
+            label6.Visible = true;
+            label8.Visible = true;
+            textBox5.Visible = true;
+        }
+    }
+    
+}
